3b4f48dab198e7d660698b8b01a1211625b1a13a
Mafly <maflycehaa@gmail.com>
2025-01-30 20:14:10 +0700

    Modify dashboard samples

--- app/Filament/Pages/Admin/Dashboard.php
+++ app/Filament/Pages/Admin/Dashboard.php
@@ -4,17 +4,18 @@ namespace App\Filament\Pages\Admin;
 
 use Filament\Pages\Dashboard as BaseDashboard;
 use Filament\Pages\Page;
-use Filament\Forms\Components\Card;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Form;
 
 class Dashboard extends Page
 {
     protected static ?string $navigationIcon = 'tabler-layout-dashboard';
-
     protected static string $view = 'filament.pages.admin.dashboard';
-
     protected ?string $heading = 'Welcome to Billmora!';
 
+    public function form(Form $form): Form
+    {
+        return $form->schema([
+            // ...
+        ]);
+    }
 }
--- resources/views/filament/pages/admin/dashboard.blade.php
+++ resources/views/filament/pages/admin/dashboard.blade.php
@@ -1,3 +1,38 @@
 <x-filament-panels::page>
-    <!--  -->
+    <x-filament::grid default="1" sm="2" class="gap-4">
+        <x-filament::card>
+            <div class="flex items-center justify-between gap-2">
+                <div class="flex items-center gap-2">
+                    <x-filament::icon icon="tabler-heart-filled" class="w-16 h-full text-danger-600"/>
+                    <div class="flex flex-col">
+                        <span class="text-lg font-semibold">Support Billmora :3</span>
+                        <span class="text-sm">Whether big or small, every contribution helps us improve and continue providing value to everyone.</span>
+                    </div>
+                </div>
+                <x-filament::button size="lg" color="danger" href="https://billmora.com/donate" tag="a" target="_blank">
+                    <div class="flex items-center gap-2">
+                        <x-filament::icon icon="tabler-cash" class="w-5 h-5"/>
+                        <span>Donate</span>
+                    </div>
+                </x-filament::button>
+            </div>
+        </x-filament::card>
+        <x-filament::card>
+            <div class="flex items-center justify-between gap-2">
+                <div class="flex items-center gap-2">
+                    <x-filament::icon icon="tabler-book" class="w-16 h-full text-primary-600"/>
+                    <div class="flex flex-col">
+                        <span class="text-lg font-semibold">Dcoumentation</span>
+                        <span class="text-sm">Make sure to read the documentation to understand and utilize the available features.</span>
+                    </div>
+                </div>
+                <x-filament::button size="lg" color="primary" href="https://docs.billmora.com/" tag="a" target="_blank">
+                    <div class="flex items-center gap-2">
+                        <x-filament::icon icon="tabler-cash" class="w-5 h-5"/>
+                        <span>Donate</span>
+                    </div>
+                </x-filament::button>
+            </div>
+        </x-filament::card>
+    </x-filament::grid>
 </x-filament-panels::page>
