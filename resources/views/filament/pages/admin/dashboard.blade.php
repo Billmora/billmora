63f508299c46aa562a21a46b8a7a11c529dbd7bf
Mafly <maflycehaa@gmail.com>
2025-02-01 00:09:56 +0700

    refactor(dashboard): Modify icon and text

--- resources/views/filament/pages/admin/dashboard.blade.php
+++ resources/views/filament/pages/admin/dashboard.blade.php
@@ -20,16 +20,16 @@
         <x-filament::card>
             <div class="flex items-center justify-between gap-2">
                 <div class="flex items-center gap-2">
-                    <x-filament::icon icon="tabler-book" class="w-16 h-full text-primary-600"/>
+                    <x-filament::icon icon="tabler-book-2" class="w-16 h-full text-primary-600"/>
                     <div class="flex flex-col">
-                        <span class="text-lg font-semibold">Dcoumentation</span>
+                        <span class="text-lg font-semibold">Documentation</span>
                         <span class="text-sm">Make sure to read the documentation to understand and utilize the available features.</span>
                     </div>
                 </div>
                 <x-filament::button size="lg" color="primary" href="https://docs.billmora.com/" tag="a" target="_blank">
                     <div class="flex items-center gap-2">
-                        <x-filament::icon icon="tabler-cash" class="w-5 h-5"/>
-                        <span>Donate</span>
+                        <x-filament::icon icon="tabler-book" class="w-5 h-5"/>
+                        <span>Read</span>
                     </div>
                 </x-filament::button>
             </div>
