1f15c298e31fae3899e4ebfe39a50965c17bdd7d
Mafly <maflycehaa@gmail.com>
2025-02-01 11:10:28 +0700

    feat: Added Config alias so that it can use Config globally

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\Setting;
+use App\Helpers\Config;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // 
+        //
     }
 
     /**
@@ -20,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // 
+        $this->app->singleton('Config', function () {
+            return new Config();
+        });
     }
 }
