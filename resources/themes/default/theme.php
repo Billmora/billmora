aada14ae3ae561d265faf67986dbafa9fb8edb21
Mafly <maflycehaa@gmail.com>
2025-02-02 18:55:50 +0700

    feat: add assets for theme

--- resources/themes/default/theme.php
+++ resources/themes/default/theme.php
@@ -5,4 +5,7 @@ return [
     'version' => '1.0.0',
     'author' => 'Billmora',
     'url' => 'https://billmora.com',
+    // This path of Theme assets like css, js, image, etc. Locate in public folder
+    // Note: make sure 'default' is the same as the Theme folder
+    'assets' => '/assets/themes/default/',
 ];
