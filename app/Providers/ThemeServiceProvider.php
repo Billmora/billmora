187484b4ecad86486407c255bd452903bce5ab93
Mafly <maflycehaa@gmail.com>
2025-02-02 16:36:15 +0700

    feat: Add theme logic view

---
+++ app/Providers/ThemeServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Helpers\Config;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $theme = session('theme',  Config::setting('company_theme', 'default'));
+
+        $themePath = resource_path("views/theme/$theme");
+
+        if (File::isDirectory($themePath)) {
+            View::addLocation($themePath);
+        }
+    }
+}
