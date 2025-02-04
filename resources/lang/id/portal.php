c245b1d34a32ca902f7e5585c39e201f29d114f8
Mafly <maflycehaa@gmail.com>
2025-02-04 15:17:11 +0700

    feat: added indonesian language

---
+++ resources/lang/id/portal.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'sign_in' => 'Masuk',
+    'sign_up' => 'Daftar',
+    'homepage' => 'Beranda',
+    'store' => 'Toko',
+    'news' => 'Berita',
+    'tos' => 'Ketentuan Layanan',
+    'toc' => 'Syarat dan Ketentuan',
+    'privacy_policy' => 'Kebijakan Privasi',
+    'modal_preference' => 'Preferensi',
+    'modal_cancel' => 'Batan',
+    'modal_save' => 'Simpan',
+    'support' => 'Bantuan',
+    'ticket' => 'Tiket',
+    'company' => 'Perusahaan',
+];
