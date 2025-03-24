82fd65dfac4325f414425bdc93aa51e012dd6c53
Mafly <maflycehaa@gmail.com>
2025-03-24 21:30:10 +0700

    feat: Add General setting page

---
+++ app/Filament/Pages/Admin/Settings/General.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Pages\Admin\Settings;
+
+use Filament\Pages\Page;
+use Filament\Navigation\NavigationItem;
+
+class General extends Page
+{
+    protected static ?string $navigationIcon = 'tabler-nut';
+    protected static string $view = 'filament.pages.admin.settings.general';
+    protected static ?string $slug = 'settings/general';
+    protected ?string $subheading = 'Configure a general settings.';
+
+    public static function getNavigationItems(): array
+    {
+        return [
+            NavigationItem::make('Settings')
+                ->url('/admin/settings')
+                ->icon('tabler-settings')
+                ->isActiveWhen(fn () => request()->is('admin/settings*'))
+                ->sort(1),
+        ];
+    }
+}
