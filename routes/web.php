035c2c9210bce2789fe66462e1892da82f81bbc1
Mafly <maflycehaa@gmail.com>
2025-04-07 15:43:33 +0700

    refactor: remove portal middleware and change to PortalController

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Portal;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [Portal\IndexController::class, 'index'])->middleware('portal.check');
+Route::get('/', [Portal\PortalController::class, 'index'])->name('portal.index');
 
 Route::group(['prefix' => '/preference'], function () {
     Route::post('/language', [Portal\PreferenceController::class, 'setLanguage'])->name('preference.language');
