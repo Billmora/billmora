8b069768b842eaa121598e264e49676fd373bb30
Mafly <maflycehaa@gmail.com>
2025-06-10 08:52:36 +0700

    feat: add auth captcha active seeder

--- database/seeders/AuthSettingSeeder.php
+++ database/seeders/AuthSettingSeeder.php
@@ -17,6 +17,7 @@ class AuthSettingSeeder extends Seeder
             ['category' => 'auth', 'key' => 'user_verified', 'value' => true],
             ['category' => 'auth', 'key' => 'form_disable', 'value' => []],
             ['category' => 'auth', 'key' => 'form_required', 'value' => ["street_address_1", "city", "country", "state", "postcode"]],
+            ['category' => 'auth', 'key' => 'captcha_active', 'value' => ["user_register", "user_login"]],
         ];
 
         foreach ($settings as $setting) {
