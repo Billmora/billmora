7f854d0f4d21838c524b888092d7e57f107b6294
Mafly <maflycehaa@gmail.com>
2025-06-14 21:39:50 +0700

    style: bolder route active and handler if title and route is null

--- resources/themes/client/default/components/breadcrumb.blade.php
+++ resources/themes/client/default/components/breadcrumb.blade.php
@@ -1,10 +1,14 @@
-@props(['title', 'route'])
+@props(['title' => null, 'route' => null])
 
 <div class="breadcrumb">
   <h2>{{ $title }}</h2>
   <div class="link">
     <a href="/">Portal</a>
-    <a href="/dashboard">Dashboard</a>
-    <a href="{{ $route }}">{{ $title }}</a>
+    @if($title && $route)
+      <a href="/dashboard">Dashboard</a>
+      <a class="active" href="{{ $route }}">{{ $title }}</a>
+    @else
+      <a class="active" href="/dashboard">Dashboard</a>
+    @endif
   </div>
 </div>
