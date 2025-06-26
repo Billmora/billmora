69916399240af0dfa41fc3f33a84fa6f6a85288c
Mafly <maflycehaa@gmail.com>
2025-06-26 23:47:59 +0700

    feat: add common lang config and set to header

---
+++ resources/lang/en/common.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+  'home' => 'Home',
+  'client_area' => 'Client Area',
+  'store' =>  'Store',
+  'news' => 'News',
+  'terms_of_service' => "Terms of Service",
+  'terms_of_condition' => "Terms of Conditon",
+  'privacy_policy' => "Privacy Policy",
+  'sign_in' => 'Sign In',
+  'sign_up' => 'Sign Up',
+
+  'preference_title' => 'Preference',
+  'preference_description' => 'Choose your language and currency are using.',
+  'language' => 'Language',
+
+  'save' => 'Save',
+  'cancel' => 'Cancel',
+];
