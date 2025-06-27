e9ec01c68d03d58821a079134daf4dca9577d641
Mafly <maflycehaa@gmail.com>
2025-06-27 12:55:32 +0700

    refactor: showing preload in portal

--- resources/themes/portal/tailwind/layouts/partials/preload.blade.php
+++ resources/themes/portal/tailwind/layouts/partials/preload.blade.php
@@ -4,8 +4,8 @@
   x-transition:leave-end="opacity-0">
   <div class="flex-col gap-4 w-full flex items-center justify-center">
     <img src="{{ Billmora::getGeneral('company_logo') }}" alt="company logo" class="h-16 w-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 animate-none">
-    <div class="w-30 h-30 border-4 border-transparent text-white text-4xl animate-spin flex items-center justify-center border-t-white rounded-full">
-      <div class="w-26 h-26 border-4 border-transparent text-billmora-primary  text-2xl animate-spin flex items-center justify-center border-t-billmora-primary  rounded-full">
+    <div class="w-30 h-30 border-4 border-transparent text-white text-4xl animate-spin flex items-center justify-center border-t-billmora-primary rounded-full">
+      <div class="w-26 h-26 border-4 border-transparent text-billmora-primary  text-2xl animate-spin flex items-center justify-center border-t-billmora-primary rounded-full">
       </div>
     </div>
   </div>
