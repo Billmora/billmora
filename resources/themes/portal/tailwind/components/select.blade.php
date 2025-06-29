a7aae05e84457b1fcc493937aa56f9c1dc39410b
Mafly <maflycehaa@gmail.com>
2025-06-29 20:12:12 +0700

    refactor: add attribute required condition

--- resources/themes/portal/tailwind/components/select.blade.php
+++ resources/themes/portal/tailwind/components/select.blade.php
@@ -1,8 +1,19 @@
-@props(['label' => null, 'name' => null])
+@props([
+  'label' => null,
+  'name' => null,
+  'required' => null,
+  ])
 
 <div class="w-full">      
   @if ($label)
-    <label for="{{ $name }}" class="block text-slate-500 font-semibold mb-0.5">{{ $label }}</label>
+    <label for="{{ $name }}" class="block text-slate-500 font-semibold mb-0.5">
+      {{ $label }}
+      @if ($required)
+        <span class="text-red-500">{{ __('auth.required_symbol') }}</span> 
+      @else
+        <span class="text-slate-400">{{ __('auth.optional_symbol') }}</span>
+      @endif
+    </label>
   @endif
   <div class="relative">
     <select name="{{ $name }}" id="{{ $name }}" class="w-full bg-billmora-2 text-gray-700 border-2 border-billmora-3 rounded-lg px-3 py-2 focus:outline-none focus:border-billmora-ring-1 hover:border-billmora-ring-1 appearance-none cursor-pointer">
