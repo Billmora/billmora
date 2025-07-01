bf344b649187b13eeb50a3184b3d1ff989695cf2
Mafly <maflycehaa@gmail.com>
2025-07-01 11:18:52 +0700

    feat: add updatePassword handler for Account Security setting

--- app/Http/Controllers/Client/Account/SecurityController.php
+++ app/Http/Controllers/Client/Account/SecurityController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Client\Account;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class SecurityController extends Controller
 {
@@ -13,5 +14,24 @@ class SecurityController extends Controller
         $user = Auth::user();
 
         return view('client::account.security', compact('user'));
+    }
+
+    public function updatePassword(Request $request)
+    {
+        $user = Auth::user();
+
+        $request->validate([
+            'current_password' => 'required',
+            'new_password' => 'required|min:8|confirmed',
+        ]);
+
+        if (!Hash::check($request->current_password, $user->password)) {
+            return back()->withErrors(['current_password' => __('auth.password_invalid_credentials')])->withInput();
+        }
+
+        $user->password = Hash::make($request->new_password);
+        $user->save();
+
+        return back()->with('success', __('client.update_security_success'));
     }
 }
