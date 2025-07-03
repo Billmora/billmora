46449e426f3fd46fc7d13dab64613c0236c0f370
Mafly <maflycehaa@gmail.com>
2025-07-03 10:22:16 +0700

    feat: add updateEmail handler for Account Security setting

--- app/Http/Controllers/Client/Account/SecurityController.php
+++ app/Http/Controllers/Client/Account/SecurityController.php
@@ -22,14 +22,33 @@ class SecurityController extends Controller
 
         $request->validate([
             'current_password' => 'required',
-            'new_password' => 'required|min:8|confirmed',
+            'new_password' => 'required|string|min:8|confirmed',
         ]);
 
         if (!Hash::check($request->current_password, $user->password)) {
-            return back()->withErrors(['current_password' => __('auth.password_invalid_credentials')])->withInput();
+            return back()->withErrors(['current_password' => __('auth.password_current_mismatch')])->withInput();
         }
 
         $user->password = Hash::make($request->new_password);
+        $user->save();
+
+        return back()->with('success', __('client.update_security_success'));
+    }
+
+    public function updateEmail(Request $request)
+    {
+        $user = Auth::user();
+
+        $request->validate([
+            'new_email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required',
+        ]);
+
+        if (!Hash::check($request->password, $user->password)) {
+            return back()->withErrors(['password' => __('auth.password_current_mismatch')])->withInput();
+        }
+
+        $user->email = $request->new_email;
         $user->save();
 
         return back()->with('success', __('client.update_security_success'));
