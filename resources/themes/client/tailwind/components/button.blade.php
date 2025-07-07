ebecc1ac5c155c0f063023e95e360cb23615ad62
Mafly <maflycehaa@gmail.com>
2025-07-07 15:46:28 +0700

    feat: add variant danger

--- resources/themes/client/tailwind/components/button.blade.php
+++ resources/themes/client/tailwind/components/button.blade.php
@@ -61,6 +61,26 @@
       {{ $slot }}
     </button>
     @break
+  @case('danger')
+    <button
+      {{ $attributes->class("
+        flex items-center gap-2 px-3 py-2 rounded-lg text-white transition-colors duration-300 ease-in-out cursor-pointer focus:outline-none focus:ring-3 focus:ring-billmora-ring-1
+        " . ($active
+            ? ' bg-red-500 text-white'
+            : ' bg-red-500 hover:bg-red-400 hover:text-white')
+        )
+      }}
+      @if ($modal)
+        x-data
+        x-on:click="$store.modal.show('{{ $modal }}')"
+      @endif
+    >
+      @if ($icon)
+        <x-dynamic-component :component="$icon" class="h-5.25 w-auto" />
+      @endif
+      {{ $slot }}
+    </button>
+    @break
   @default
     <button
       {{ $attributes->class("
