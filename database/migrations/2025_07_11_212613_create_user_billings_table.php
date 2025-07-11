5ee182fab3782a559eec7956d5f38a6d77594434
Mafly <maflycehaa@gmail.com>
2025-07-11 21:34:07 +0700

    refactor(migrations): split users table schema into multiple migration files

---
+++ database/migrations/2025_07_11_212613_create_user_billings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_billings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('phone_number')->nullable();
+            $table->string('company_name')->nullable();
+            $table->string('street_address_1')->nullable();
+            $table->string('street_address_2')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->string('state')->nullable();
+            $table->string('postcode')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_billings');
+    }
+};
