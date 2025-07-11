67e3d6e505e5e5d0ac8b0558e97805a039c516a9
Mafly <maflycehaa@gmail.com>
2025-07-11 18:30:19 +0700

    feat: add modal for disable TwoFactor and set button with condition TwoFactor enable or disable

--- resources/themes/client/tailwind/account/security.blade.php
+++ resources/themes/client/tailwind/account/security.blade.php
@@ -11,6 +11,21 @@
     <x-client::alert variant="danger" icon="lucide-triangle-alert" description="{{ session('error') }}" />
   </div>
 @endif
+@error('totp')
+  <div class="w-full flex mx-auto mb-6">
+    <x-client::alert variant="danger" icon="lucide-triangle-alert" description="{{ $message }}" />
+  </div>
+@enderror
+<x-client::modal modal="2faDisableModal" title="{{ __('client.2fa_title') }}" description="{{ __('common.disable_alert', ['title' => __('client.2fa_title')]) }}"  icon="lucide-shield-alert" variant="danger" position="centered">
+  <form action="{{ route('client.two-factor.disable') }}" method="POST">
+    @csrf
+    <x-input type="text" name="totp" label="{{ __('client.2fa_totp') }}" required />
+    <div class="flex justify-end gap-2 mt-4">
+      <x-button type="button" x-on:click="$store.modal.close()" variant="secondary">{{ __('common.cancel') }}</x-button>
+      <x-button type="submit" variant="danger">{{ __('common.disable') }}</x-button>
+    </div>
+  </form>
+</x-client::modal>
 <div class="flex flex-col md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-3">
   <form action="{{ route('client.account.security.password.update') }}" method="POST" class="w-full h-h-fit bg-billmora-2 p-6 rounded-lg border-3 border-billmora-3">
     @csrf
@@ -40,7 +55,11 @@
       <h3 class="text-xl text-slate-600 font-bold">{{ __('client.2fa_title') }}</h3>
       <div class="space-y-2">
         <p class="text-slate-500">{{ __('client.2fa_description') }}</p>
-        <x-client::link href="/auth/two-factor/setup" variant="primary" icon="lucide-check" class="w-fit ml-auto font-semibold">{{ __('common.enable') }}</x-client::link>
+        @if (auth()->user()->twoFactor && auth()->user()->twoFactor->enabled)
+          <x-client::button modal="2faDisableModal" variant="danger" icon="lucide-x" class="w-fit ml-auto font-semibold">{{ __('common.disable') }}</x-client::button>
+        @else
+          <x-client::link href="/auth/two-factor/setup" variant="primary" icon="lucide-check" class="w-fit ml-auto font-semibold">{{ __('common.enable') }}</x-client::link>
+        @endif
       </div>
     </div>
   </div>
