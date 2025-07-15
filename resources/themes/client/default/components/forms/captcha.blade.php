9a4ea5feccc29c66ac496f130b0a6de82775fcc0
Mafly <maflycehaa@gmail.com>
2025-07-15 14:26:37 +0700

    refactor: clean up and remove unused default theme

--- resources/themes/client/default/components/forms/captcha.blade.php
+++
@@ -1,7 +0,0 @@
-@props(['form' => null])
-
-@if (\App\Services\CaptchaService::enabled($form))
-    <div class="form-group">
-        {!! \App\Services\CaptchaService::render() !!}
-    </div>
-@endif
