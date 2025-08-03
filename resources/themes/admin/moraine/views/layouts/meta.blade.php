beccf9e7e6a8daca34ac41c1ac8f28bcd5336254
Mafly <maflycehaa@gmail.com>
2025-08-03 15:48:47 +0700

    feat: add livewire styles

--- resources/themes/admin/moraine/views/layouts/meta.blade.php
+++ resources/themes/admin/moraine/views/layouts/meta.blade.php
@@ -3,6 +3,7 @@
 {{-- Meta --}}
 <link rel="icon" href="https://media.billmora.com/logo/main-bgnone.svg">
 <title>Admin Dashboard | Billmora</title>
-<!-- Styles / Tailwind / Vite -->
+<!-- Styles -->
 <link rel="stylesheet" href="{{ $adminTheme['assets'] }}/css/style.css">
 <script src="{{ $adminTheme['assets'] }}/js/app.js" type="module"></script>
+@livewireStyles
