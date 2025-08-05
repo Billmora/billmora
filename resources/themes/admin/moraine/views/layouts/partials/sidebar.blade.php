d667a7bf58a1e55496b03241b44eb1a80d78f281
Mafly <maflycehaa@gmail.com>
2025-08-05 11:20:42 +0700

    refactor: remove index from route name

--- resources/themes/admin/moraine/views/layouts/partials/sidebar.blade.php
+++ resources/themes/admin/moraine/views/layouts/partials/sidebar.blade.php
@@ -11,8 +11,8 @@
     <hr class="border-t-2 border-billmora-2 my-7 mr-8">
     <div class="space-y-2 overflow-y-auto pr-6" id="sidemenu">
       {{-- Sidebar content --}}
-      <a href="{{ route('admin.dashboard.index') }}" class="flex gap-2 items-center hover:bg-billmora-primary px-3 py-3 rounded-lg text-slate-600 hover:text-white transition-colors duration-300">
-        <x-lucide-layout-dashboard class="w-5 h-auto stroke-[2.25px]" />
+      <a href="{{ route('admin.dashboard') }}" class="flex gap-2 items-center hover:bg-billmora-primary px-3 py-3 rounded-lg text-slate-600 hover:text-white transition-colors duration-300">
+        <x-lucide-layout-grid class="w-5 h-auto" />
         <span class="font-semibold">{{ __('admin/navigation.dashboard') }}</span>
       </a>
       <span class="mt-4 block text-slate-600 font-semibold text-md">{{ __('admin/navigation.group.system') }}</span>
--- routes/web/admin.php
+++ routes/web/admin.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Admin;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin'], function () {
-  Route::get('/', [Admin\DashboardController::class, 'index'])->name('admin.dashboard.index');
+  Route::get('/', [Admin\DashboardController::class, 'index'])->name('admin.dashboard');
 
   // Global command-bar search endpoint (used by AlpineJS component in admin UI)
   Route::get('/quick-search', [Admin\QuickSearchController::class, 'search'])->name('admin.quick-search');
