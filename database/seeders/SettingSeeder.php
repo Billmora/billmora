da33dc33dd1f4985528e827f851af46d20885373
Mafly <maflycehaa@gmail.com>
2025-08-07 17:19:08 +0700

    feat: add database settings seeder file

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class SettingSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     *
+     * Seeds initial data into the 'settings' table.
+     * This seeder can be used to populate default application settings.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        // 
+    }
+}
