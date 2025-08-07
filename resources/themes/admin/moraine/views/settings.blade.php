d704da213e8a75fb1405f4695515ff2dd2ab800f
Mafly <maflycehaa@gmail.com>
2025-08-07 22:07:37 +0700

    refactor(style): remove grid cols 4

--- resources/themes/admin/moraine/views/settings.blade.php
+++ resources/themes/admin/moraine/views/settings.blade.php
@@ -1,7 +1,7 @@
 @extends('admin::layouts.app')
 
 @section('body')
-<div class="grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+<div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
     <a href="{{ route('admin.settings.general') }}" class="flex gap-4 items-center bg-white p-4 border-2 border-billmora-2 hover:border-billmora-primary rounded-2xl transition ease-in-out duration-150">
         <div class="bg-billmora-primary p-2 rounded-full">
             <x-lucide-bolt class="w-auto h-10 text-white" />
