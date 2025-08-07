de5013c42527b3ceae2e1ed34385d91e2ff9adea
Mafly <maflycehaa@gmail.com>
2025-08-07 23:28:41 +0700

    refactor(style): custom chevrons without using relative element

--- resources/themes/admin/moraine/views/components/select.blade.php
+++ resources/themes/admin/moraine/views/components/select.blade.php
@@ -20,7 +20,7 @@
         </div>
     @endif
 
-    <div class="relative my-1">
+    <div class="flex items-center w-full my-1">
         <select name="{{ $name }}" id="{{ $name }}"
             x-on:select="errorVisible = false"
             :class="[
@@ -31,8 +31,7 @@
             <option class="text-slate-500" selected disabled>{{ __('admin/common.choose_option') }}</option>
             {{ $slot }}
         </select>
-        <x-lucide-chevrons-up-down
-            class="w-auto h-5 absolute top-1/2 right-2 -translate-y-1/2 text-slate-700 pointer-events-none" />
+        <x-lucide-chevrons-up-down class="w-auto h-5 -ml-7 text-slate-700 pointer-events-none" />
     </div>
 
     <template x-if="errorVisible">
