6f0901a86340eed4ace282c0b1faf4714cec35dc
Mafly <maflycehaa@gmail.com>
2025-08-07 22:59:28 +0700

    refactor: remove placeholder attribute

--- resources/themes/admin/moraine/views/components/select.blade.php
+++ resources/themes/admin/moraine/views/components/select.blade.php
@@ -21,7 +21,7 @@
     @endif
 
     <div class="relative my-1">
-        <select name="{{ $name }}" id="{{ $name }}" placeholder="anjay"
+        <select name="{{ $name }}" id="{{ $name }}"
             x-on:select="errorVisible = false"
             :class="[
                 'w-full text-slate-700 rounded-lg px-3 py-2.5 border-2 border-billmora-2 outline-none focus:ring-2 ring-billmora-primary appearance-none cursor-pointer',
