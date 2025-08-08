0e49989bdff8fc2a50d2129ca7a2f40756fc5fd0
Mafly <maflycehaa@gmail.com>
2025-08-08 20:57:44 +0700

    fix(toggle): removing props value and set toggle value default 1

--- resources/themes/admin/moraine/views/components/toggle.blade.php
+++ resources/themes/admin/moraine/views/components/toggle.blade.php
@@ -1,7 +1,6 @@
 @props([
     'name',
     'label' => null,
-    'value' => null,
     'error' => $errors->first($name),
     'required' => null,
     'helper' => null,
@@ -21,7 +20,7 @@
 
     <label class="inline-flex items-center cursor-pointer">
         <input type="hidden" name="{{ $name }}" value="0">
-        <input type="checkbox" name="{{ $name }}" id="{{ $name }}" value="{{ $value }}"
+        <input type="checkbox" name="{{ $name }}" id="{{ $name }}" value="1"
             class="sr-only peer" @checked($checked)>
         <div
             class="relative w-11 h-6 bg-billmora-2 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-billmora-primary after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-billmora-2 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-billmora-primary">
