27368a9299dfd9ee3f0f4ea318e9c84a629104a4
Mafly <maflycehaa@gmail.com>
2025-08-08 16:33:06 +0700

    refactor(component): remove attribute and using checked props

--- resources/themes/admin/moraine/views/components/radio/option.blade.php
+++ resources/themes/admin/moraine/views/components/radio/option.blade.php
@@ -2,6 +2,7 @@
     'name',
     'label' => null,
     'value' => old($name),
+    'checked' => false,
 ])
 
 <div class="w-full inline-flex items-center gap-2">
@@ -11,11 +12,10 @@
             id="{{ $value }}"
             value="{{ $value }}"
             class="peer h-5 w-5 cursor-pointer appearance-none rounded-full border-3 border-billmora-2 checked:border-billmora-primary transition-all" 
-            {{ $attributes }} />
+            @checked($checked) />
     
         <span
             class="absolute bg-billmora-primary w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
-    
     </label>
     <label class="text-slate-500 cursor-pointer" for="{{ $value }}">{{ $label }}</label>
 </div>
