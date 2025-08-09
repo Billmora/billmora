8ac9ba7d9a398cbe496b99da3de62e161e3e4073
Mafly <maflycehaa@gmail.com>
2025-08-09 16:50:22 +0700

    fix(option): ensure old value selection works

--- resources/themes/admin/moraine/views/components/radio/option.blade.php
+++ resources/themes/admin/moraine/views/components/radio/option.blade.php
@@ -12,7 +12,7 @@
             id="{{ $value }}"
             value="{{ $value }}"
             class="peer h-5 w-5 cursor-pointer appearance-none rounded-full border-3 border-billmora-2 checked:border-billmora-primary transition-all" 
-            @checked($checked) />
+            @checked(old($name) !== null ? old($name) == $value : $checked) />
     
         <span
             class="absolute bg-billmora-primary w-3 h-3 rounded-full opacity-0 peer-checked:opacity-100 transition-opacity duration-200 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
