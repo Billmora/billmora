2a4f79449663c033a5634dcec21b6e15079f05fc
Mafly <maflycehaa@gmail.com>
2025-08-12 20:23:06 +0700

    refactor(tabs): improve tab component with pre-routed links and translation support

--- resources/themes/admin/moraine/views/components/tabs.blade.php
+++ resources/themes/admin/moraine/views/components/tabs.blade.php
@@ -5,11 +5,11 @@
 
 <div class="flex gap-4 bg-white w-full p-4 border-2 border-billmora-2 rounded-2xl overflow-x-auto">
     @foreach ($tabs as $tab)
-        <a href="{{ route($tab['route']) }}"
+        <a href="{{ $tab['route'] }}"
            class="flex items-center gap-2 px-3 py-2 rounded-lg transition ease-in-out duration-150
                   {{ $active === $tab['route'] ? 'bg-billmora-primary text-white' : 'hover:bg-billmora-primary text-slate-700 hover:text-white' }}">
             <x-dynamic-component :component="$tab['icon']" class="w-auto h-5" />
-            <span>{{ __($tab['label']) }}</span>
+            <span>{{ $tab['label'] }}</span>
         </a>
     @endforeach
 </div>
