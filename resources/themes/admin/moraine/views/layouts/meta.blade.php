5f31e35978ac44aa0a628260114a3077ae050927
Mafly <maflycehaa@gmail.com>
2025-08-12 18:21:38 +0700

    feat: add froala as wysiwyg editor

--- resources/themes/admin/moraine/views/layouts/meta.blade.php
+++ resources/themes/admin/moraine/views/layouts/meta.blade.php
@@ -6,4 +6,6 @@
 <!-- Styles -->
 <link rel="stylesheet" href="{{ $adminTheme['assets'] }}/css/style.css">
 <script src="{{ $adminTheme['assets'] }}/js/app.js" type="module"></script>
+{{-- Frola Editor --}}
+<link href='https://cdn.jsdelivr.net/npm/froala-editor@latest/css/froala_editor.pkgd.min.css' rel='stylesheet' type='text/css' />
 @livewireStyles
--- resources/themes/admin/moraine/views/layouts/script.blade.php
+++ resources/themes/admin/moraine/views/layouts/script.blade.php
@@ -34,3 +34,22 @@
         });
     });
 </script>
+<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/froala-editor@latest/js/froala_editor.pkgd.min.js'></script>
+@foreach ($langs as $lang)
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/4.3.1/js/languages/{{ explode('_', $lang['lang'])[0] }}.min.js"></script>
+@endforeach
+<script>
+    let editor = new FroalaEditor('#froalaEditor', {
+        language: '{{ explode('_', $langActive['lang'])[0] }}',
+        toolbarButtons: [
+          'bold', 'italic', 'underline', 'strikeThrough', 'fontSize', 'color', 'paragraphStyle', 'paragraphFormat',
+          'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'insertLink', 'insertImage', 'insertTable',
+          'emoticons', 'specialCharacters', 'insertHR', 'selectAll', 'help', 'fullscreen',
+        ],
+        quickInsertTags: [],
+        height: 500,
+        spellcheck: false,
+        imageUpload: true,
+        charCounterCount: true,
+    });
+</script>
