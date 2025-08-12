52990df3e62bce0355661179aabb5a8d948ead0e
Mafly <maflycehaa@gmail.com>
2025-08-12 18:38:08 +0700

    feat(components): add froala wysiwyg editor

---
+++ resources/themes/admin/moraine/views/components/editor/text.blade.php
@@ -0,0 +1,32 @@
+@props([
+    'name',
+    'label' => null,
+    'error' => $errors->first($name),
+    'required' => null,
+    'helper' => null,
+])
+
+<div x-data="{ errorVisible: {{ $error ? 'true' : 'false' }} }" class="w-full">
+    @if ($label)
+        <div class="flex gap-1">
+            <label for="{{ $name }}" class="block text-slate-600 font-semibold mb-0.5">
+                {{ $label }}
+            </label>
+            <span class="text-slate-600">{{ $required ? __('admin/common.symbol_required') : __('admin/common.symbol_optional') }}</span>
+        </div>
+    @endif
+
+    <div x-on:input="errorVisible = false"
+        :class="[
+            'w-full text-slate-700 rounded-lg my-1',
+            errorVisible ? 'fr-error' : ''
+        ]">
+        <textarea id="froalaEditor" name="{{ $name }}" id="{{ $name }}" {{ $attributes }}>{{ $slot }}</textarea>
+    </div>
+
+    @if ($error)
+        <p class="mt-1 text-sm text-red-400 font-semibold" x-show="errorVisible">{{ $error }}</p>
+    @elseif ($helper)
+        <p class="mt-1 text-sm text-slate-500">{{ $helper }}</p>
+    @endif
+</div>
