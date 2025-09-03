3f7775069edc78734234f6246b70a853ca6535e6
Mafly <maflycehaa@gmail.com>
2025-09-03 21:04:16 +0700

    feat(meta): add custom title and company name into title tag

--- resources/themes/admin/moraine/views/layouts/meta.blade.php
+++ resources/themes/admin/moraine/views/layouts/meta.blade.php
@@ -2,7 +2,7 @@
 <meta name="robots" content="noindex, nofollow">
 {{-- Meta --}}
 <link rel="icon" href="https://media.billmora.com/logo/main-bgnone.svg">
-<title>Admin Dashboard | Billmora</title>
+<title>@yield('title', 'Admin') | {{ Billmora::getGeneral('company_name') }}</title>
 <!-- Styles -->
 <link rel="stylesheet" href="{{ $adminTheme['assets'] }}/css/style.css">
 <script src="{{ $adminTheme['assets'] }}/js/app.js" type="module"></script>
