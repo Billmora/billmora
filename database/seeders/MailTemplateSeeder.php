2b48e4834544fb8a9ca92c64f361f3fdee82a35e
Mafly <maflycehaa@gmail.com>
2025-09-04 23:58:20 +0700

    feat(seeder): add user registration into mail template

--- database/seeders/MailTemplateSeeder.php
+++ database/seeders/MailTemplateSeeder.php
@@ -35,6 +35,36 @@ class MailTemplateSeeder extends Seeder
                     ],
                 ],
             ],
+            [
+                'key' => 'user_registration',
+                'name' => 'User Registration',
+                'placeholder' => [
+                    'client_name' => 'Client name',
+                    'company_name' => 'Company name',
+                    'verify_url' => 'Email verification URL',
+                    'client_url' => 'Client Area URL',
+                ],
+                'translations' => [
+                    'en_US' => [
+                        'subject' => 'Account has been created, verify your Email',
+                        'body' => <<<HTML
+                            <p>Hello, {client_name}!</p>
+                            <p>Welcome to {company_name} and thank you for registering with us!</p>
+                            <br />
+                            <p>Please click on the link below to verify your email address. This is required to confirm ownership of the email address.</p>
+                            <a href="{verify_url}" target="_blank">Verify Email</a>
+                            <br /><br />
+                            <p>If you're having trouble, try copying and pasting the following URL into your browser:</p>
+                            <a href="{verify_url}" target="_blank">{verify_url}</a>
+                            <br /><br />
+                            <p>This link is valid for 60 minutes only. If it has expired, login to our <a href="{company_url}" target="_blank">Client Area</a> to request a new link.</p>
+                            <br />
+                            <p>Best Regards,</p>
+                            <p>{company_name}</p>
+                        HTML,
+                    ],
+                ],
+            ],
         ];
 
         foreach ($templates as $template) {
