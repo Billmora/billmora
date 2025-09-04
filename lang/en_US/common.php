cdd11aa9a44302e3101162460f09b7544c77c86d
Mafly <maflycehaa@gmail.com>
2025-09-04 23:55:49 +0700

    feat(lang): add field of user information

--- lang/en_US/common.php
+++ lang/en_US/common.php
@@ -10,8 +10,22 @@ return [
     'sign_in' => 'Sign In',
     'sign_up' => 'Sign Up',
     
+    'personal_information' => 'Personal Information',
+    'first_name' => 'First Name',
+    'last_name' => 'Last Name',
     'email' => 'Email',
+    'phone_number' => 'Phone Number',
+    'billing_information' => 'Billing Information',
+    'company_name' => 'Company Name',
+    'street_address_1' => 'Street Address 1',
+    'street_address_2' => 'Street Address 2',
+    'city' => 'City',
+    'country' => 'Country',
+    'state' => 'State',
+    'postcode' => 'Postcode',
+    'security_information' => 'Security Information',
     'password' => 'Password',
+    'confirm_password' => 'Confirm Password',
 
     'back_to' => 'Back to :page'
 ];
