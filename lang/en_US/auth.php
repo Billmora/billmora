003e395824ab699d9e291e0d14eaeb30275bf39c
Mafly <maflycehaa@gmail.com>
2025-09-04 23:45:47 +0700

    feat(lang): add lang for registration succesfull

--- lang/en_US/auth.php
+++ lang/en_US/auth.php
@@ -14,4 +14,5 @@ return [
     'have_account' => 'Already have an account?',
 
     'invalid_credentials' => 'No account matching those credentials could be found.',
+    'registration_successful ' => 'Thank you for registering. We have sent you an email for verification. Please check your inbox and follow the instructions to verify your email.',
 ];
