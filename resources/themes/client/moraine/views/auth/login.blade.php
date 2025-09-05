f0b85de40578188896c7f41ebc60513bfd2a6112
Mafly <maflycehaa@gmail.com>
2025-09-05 23:01:33 +0700

    feat(auth): insert forgot password route

--- resources/themes/client/moraine/views/auth/login.blade.php
+++ resources/themes/client/moraine/views/auth/login.blade.php
@@ -33,7 +33,7 @@
                     <div class="grid gap-3">
                         <x-client::input type="email" name="email" label="{{ __('common.email') }}" required />
                         <x-client::input type="password" name="password" label="{{ __('common.password') }}" required />
-                        <a href="#" class="text-billmora-primary hover:text-billmora-primary-hover font-semibold text-end">{{ __('auth.forgot_password') }}</a>
+                        <a href="{{ route('client.password.forgot') }}" class="text-billmora-primary hover:text-billmora-primary-hover font-semibold text-end">{{ __('auth.forgot_password') }}</a>
                     </div>
                     <button type="submit" class="bg-billmora-primary hover:bg-billmora-primary-hover px-3 py-2 text-white font-semibold rounded-lg transition duration-150 cursor-pointer">{{ __('common.sign_in') }}</button>
                     <span class="text-slate-600">{{ __('auth.dont_have_account') }} <a href="{{ route('client.register') }}" class="text-billmora-primary hover:text-billmora-primary-hover font-semibold">{{ __('common.sign_up') }}</a></span>
