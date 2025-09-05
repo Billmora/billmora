811979f4236e1ed1f70b7c0a6c398b386e1f1a50
Mafly <maflycehaa@gmail.com>
2025-09-05 22:37:22 +0700

    feat(seeder): add user_password_reset into MailTemplateSeeder

--- database/seeders/MailTemplateSeeder.php
+++ database/seeders/MailTemplateSeeder.php
@@ -94,6 +94,37 @@ class MailTemplateSeeder extends Seeder
                     ],
                 ],
             ],
+            [
+                'key' => 'user_password_reset',
+                'name' => 'User Password Reset',
+                'placeholder' => [
+                    'client_name' => 'Client name',
+                    'company_name' => 'Company name',
+                    'reset_url' => 'Password reset URL',
+                    'clientarea_url' => 'Client Area URL',
+                ],
+                'translations' => [
+                    'en_US' => [
+                        'subject' => 'Reset Your Password',
+                        'body' => <<<HTML
+                            <p>Hello, {client_name}!</p>
+                            <br />
+                            <p>If you did not request a password reset, please ignore this email. Your password will remain unchanged.</p>
+                            <br />
+                            <p>Please click the link below to set a new password.</p>
+                            <a href="{reset_url}" target="_blank">Reset Password</a>
+                            <br /><br />
+                            <p>If you're having trouble, try copying and pasting the following URL into your browser:</p>
+                            <a href="{reset_url}" target="_blank">{reset_url}</a>
+                            <br /><br />
+                            <p>This link is valid for 60 minutes only. If it has expired, login to our <a href="{clientarea_url}" target="_blank">Client Area</a> to request a new link.</p>
+                            <br />
+                            <p>Best Regards,</p>
+                            <p>{company_name}</p>
+                        HTML,
+                    ],
+                ],
+            ],
         ];
 
         foreach ($templates as $template) {
