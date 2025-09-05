6839411803e2369448af0357bded906b2e74e237
Mafly <maflycehaa@gmail.com>
2025-09-05 16:54:47 +0700

    feat(lang): add for email authentication

--- lang/en_US/auth.php
+++ lang/en_US/auth.php
@@ -15,4 +15,15 @@ return [
 
     'invalid_credentials' => 'No account matching those credentials could be found.',
     'registration_successful' => 'Thank you for registering. We have sent you an email for verification. Please check your inbox and follow the instructions to verify your email.',
+
+    'email' => [
+        'not_verified' => 'Your email has not been verified. Please check your inbox or resend the verification email.',
+        'has_verified' => 'Your email has been successfully verified. You can login now.',
+        'already_verified' => 'Your email is already verified. Please login.',
+        'invalid_token' => 'Invalid email verification token.',
+        'expired_token' => 'Your verification token has expired. Please request a new one.',
+        'already_requested' => 'You have already requested a verification email. Please check your inbox.',
+        'resent' => 'We have resent a verification email to you. Please check your inbox.',
+        'invalid_request' => 'Invalid request token for email verification.',
+    ],
 ];
