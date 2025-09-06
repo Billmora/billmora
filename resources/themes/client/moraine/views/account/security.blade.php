3dc86f54412d5e8db055b820a53ae6e38902da50
Mafly <maflycehaa@gmail.com>
2025-09-06 23:30:20 +0700

    feat(account): add two-factor authentication enable and disable

--- resources/themes/client/moraine/views/account/security.blade.php
+++ resources/themes/client/moraine/views/account/security.blade.php
@@ -5,9 +5,9 @@
     @if (session('success'))
         <x-client::alert variant="success">{{ session('success') }}</x-client::alert>
     @endif
-    @if (session('warning'))
-        <x-client::alert variant="warning">{{ session('warning') }}</x-client::alert>
-    @endif
+    @error('totp')
+    <x-client::alert variant="danger">{{ $message }}</x-client::alert>
+    @enderror
     @if (session('error'))
         <x-client::alert variant="danger">{{ session('error') }}</x-client::alert>
     @endif
@@ -29,6 +29,31 @@
             <x-client::input type="password" name="new_password_confirmation" label="{{ __('client/account.confirm_new_password') }}" required />
             <button type="submit" class="bg-billmora-primary hover:bg-billmora-primary-hover ml-auto px-3 py-2 text-white font-semibold rounded-lg transition duration-150 cursor-pointer">{{ __('common.update') }}</button>
         </form>
+        <div class="w-full h-fit grid gap-4 bg-white p-8 border-2 border-billmora-2 rounded-xl">
+            <h3 class="text-xl font-semibold text-slate-700">{{ __('auth.2fa.title') }} </h3>
+            <p class="text-slate-500">{{ __('auth.2fa.description') }}</p>
+            @if ($user->twoFactor && $user->twoFactor->isActive())
+                <x-client::modal.trigger modal="2faDisableModal" class="bg-red-500 border-2 border-red-500 hover:bg-red-600 ml-auto px-3 py-2 text-white rounded-lg transition-colors ease-in-out duration-150 cursor-pointer">{{ __('common.disable') }}</x-client::modal.trigger>
+            @else
+                <a href="{{ route('client.two-factor.setup') }}" variant="primary" icon="lucide-check" class="bg-billmora-primary hover:bg-billmora-primary-hover ml-auto px-3 py-2 text-white font-semibold rounded-lg transition duration-150 cursor-pointer">{{ __('common.enable') }}</a>
+            @endif
+        </div>
     </div>
 </div>
+<x-client::modal.content
+    modal="2faDisableModal"
+    variant="danger"
+    size="lg"
+    position="centered"
+    title="{{ __('common.disable_modal_title') }}"
+    description="{{ __('common.disable_modal_description', ['item' => __('auth.2fa.title')]) }}">
+    <form action="{{ route('client.two-factor.disable') }}" method="POST">
+        @csrf
+        <x-input type="text" name="totp" label="{{ __('auth.2fa.verify.totp') }}" required />
+        <div class="flex justify-end gap-2 mt-4">
+            <x-client::modal.trigger type="button" variant="close" class="bg-billmora-1 border-2 border-billmora-primary hover:bg-billmora-primary-hover px-3 py-2 text-billmora-primary hover:text-white rounded-lg transition-colors ease-in-out duration-150 cursor-pointer">{{ __('common.cancel') }}</x-client::modal.trigger>
+            <button type="submit" class="bg-red-500 border-2 border-red-500 hover:bg-red-600 px-3 py-2 text-white rounded-lg transition-colors ease-in-out duration-150 cursor-pointer">{{ __('common.disable') }}</button>
+        </div>
+    </form>
+</x-client::modal.content>
 @endsection
