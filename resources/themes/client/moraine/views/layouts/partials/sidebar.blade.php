c7628d44a3da9d990014c47869d911774904e457
Mafly <maflycehaa@gmail.com>
2025-09-06 14:23:28 +0700

    feat(navigation): insert route client dashboard

--- resources/themes/client/moraine/views/layouts/partials/sidebar.blade.php
+++ resources/themes/client/moraine/views/layouts/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <nav id="sidebar" class="fixed z-10 xl:sticky top-0 left-0 xl:block shrink-0 p-5 xl:pr-0 w-[350px] sm:w-[400px] h-dvh -translate-x-full xl:translate-x-0 transition-transform duration-300 ease-in-out">
   <div class="bg-white flex flex-col w-full h-full border-2 border-billmora-2 rounded-2xl p-8 pr-0">
-    <a href="#" class="relative flex gap-3 items-center mr-8">
+    <a href="{{ route('client.dashboard') }}" class="relative flex gap-3 items-center mr-8">
       <img src="{{ Billmora::getGeneral('company_logo') }}" alt="billmora logo" class="w-auto h-11 rounded-lg">
       <h3 class="text-2xl font-extrabold uppercase text-billmora-primary">Billmora</h3>
       {{-- Sidebar close toggle --}}
