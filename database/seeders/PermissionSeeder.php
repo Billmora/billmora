3e182467e00b7b80941ebbc436500f8b270adf8b
Mafly <maflycehaa@gmail.com>
2025-09-10 15:58:47 +0700

    feat(seeder): add sample permission seeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        /**
+         * Run seeder of permission.
+         *
+         * Seeds initial data into the 'permission'.
+         */
+        $permissions = [
+            'settings.general.view',
+            'settings.general.update',
+            'settings.roles.view',
+            'settings.roles.create',
+            'settings.roles.update',
+            'settings.roles.delete',
+        ];
+
+        /**
+         * Insert or update each permission record.
+         */
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+    }
+}
