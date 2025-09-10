8f615deeb22c01dd7e1d907a7c958ba01d509f4d
Mafly <maflycehaa@gmail.com>
2025-09-10 15:56:49 +0700

    feat(lang): add roles and permission settings translation

---
+++ lang/en_US/admin/settings/roles.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'Role & Permission settings',
+    'description' => 'Configure role & permission settings for the system.',
+
+    'role_name_label' => 'Role Name',
+    'role_name_helper' => 'Enter a name for this role. It will be used to identify and assign permissions.',
+    'role_permissions_label' => 'Role Permissions',
+    'role_permissions_helper' => 'Select the permissions this role should have. Users assigned to this role will inherit these permissions.',
+];
