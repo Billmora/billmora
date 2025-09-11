14248c9c1070b025cacad6ed3f84e86ab854d156
Mafly <maflycehaa@gmail.com>
2025-09-12 00:21:44 +0700

    feat(navigation): add card navigation to role settings

--- resources/themes/admin/moraine/views/settings.blade.php
+++ resources/themes/admin/moraine/views/settings.blade.php
@@ -40,5 +40,14 @@
            <p class="text-slate-500">{{ __('admin/settings/captcha.description') }}</p>
         </div>
     </a>
+    <a href="{{ route('admin.settings.roles') }}" class="flex gap-4 items-center bg-white p-4 border-2 border-billmora-2 hover:border-billmora-primary rounded-2xl transition ease-in-out duration-150">
+        <div class="bg-billmora-primary p-2 rounded-full">
+            <x-lucide-shield class="w-auto h-10 text-white" />
+        </div>
+        <div>
+           <h4 class="text-lg text-slate-700 font-semibold">{{ __('admin/settings/role.title') }}</h4> 
+           <p class="text-slate-500">{{ __('admin/settings/role.description') }}</p>
+        </div>
+    </a>
 </div>
 @endsection
