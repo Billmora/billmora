0163e65810882a3e99ca4c939da0815e7e83f905
Mafly <maflycehaa@gmail.com>
2025-09-12 14:58:38 +0700

    refactor(lang): remove "and" on title and description

--- lang/en_US/admin/settings/role.php
+++ lang/en_US/admin/settings/role.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'title' => 'Role and Permission settings',
-    'description' => 'Configure role & permission settings for the system.',
+    'title' => 'Role Permission settings',
+    'description' => 'Configure role permission settings for the system.',
 
     'role_name_label' => 'Role Name',
     'role_name_helper' => 'Enter a name for this role. It will be used to identify and assign permissions.',
