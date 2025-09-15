2bd978652322faa09ea983809910902b8be45972
Mafly <maflycehaa@gmail.com>
2025-09-15 17:03:30 +0700

    docs(admin): add detailed phpdoc for index method

--- app/Http/Controllers/Admin/Users/Edit/SummaryController.php
+++ app/Http/Controllers/Admin/Users/Edit/SummaryController.php
@@ -8,6 +8,17 @@ use Illuminate\Http\Request;
 
 class SummaryController extends Controller
 {
+
+    /**
+     * Display the summary information of a specific user.
+     *
+     * @param \Illuminate\Http\Request $request The incoming HTTP request.
+     * @param int                      $id      The ID of the user to display.
+     *
+     * @return \Illuminate\View\View The view instance displaying the user summary.
+     *
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException If the user does not exist.
+     */
     public function index(Request $request, $id)
     {
         $user = User::with('billing')->findOrFail($id);
