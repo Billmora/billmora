ed2d7d1b1273b993edebcf3389d2e193698a3995
Mafly <maflycehaa@gmail.com>
2025-09-16 14:27:43 +0700

    feat(users): add manual email verification translation

--- lang/en_US/admin/users/edit.php
+++ lang/en_US/admin/users/edit.php
@@ -5,6 +5,11 @@ return [
         'summary' => 'Summary',
     ],
 
+    'email_verification_alert_label' => 'Pending Email Verification',
+    'email_verification_alert_helper' => 'This user\'s email address has not been verified yet. You may verify it manually if needed.',
+    'email_verification_alert_success' => 'Email address has been successfully verified.',
+    'marked_as_verified' => 'Marked as Verified',
+
     'login_as_user' => 'Login as User',
     'login_as_user_success' => 'You are now logged in as the user :email.',
     'login_as_user_error' => 'You cannot login as the same user you are currently logged in as.',
