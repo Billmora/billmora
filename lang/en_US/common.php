7d7fe90f436a4ad37552d26478f5d1963cfa7cc0
Mafly <maflycehaa@gmail.com>
2025-09-17 00:24:44 +0700

    feat(lang): add role translation

--- lang/en_US/common.php
+++ lang/en_US/common.php
@@ -21,6 +21,7 @@ return [
     'first_name' => 'First Name',
     'last_name' => 'Last Name',
     'email' => 'Email',
+    'role' => 'Role',
     'status' => 'Status',
     'currency' => 'Currency',
     'language' => 'Language',
