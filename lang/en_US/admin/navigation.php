f3c63eec2f4bf777f6611e0359a13d36f715f5c4
Mafly <maflycehaa@gmail.com>
2025-09-22 16:35:55 +0700

    feat(lang): add audit translation in navigation

--- lang/en_US/admin/navigation.php
+++ lang/en_US/admin/navigation.php
@@ -4,6 +4,7 @@ return [
     'dashboard' => 'Dashboard',
     'users' => 'Users',
     'settings' => 'Settings',
+    'audits' => 'Audits',
     'group' => [
         'management' => 'Management',
         'system' => 'System',
