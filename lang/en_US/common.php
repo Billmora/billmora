c2c5be596ccfb8d9e06a9c3c1b35f71b862c5303
Mafly <maflycehaa@gmail.com>
2025-09-24 13:32:38 +0700

    feat(lang): insert event translation to common

--- lang/en_US/common.php
+++ lang/en_US/common.php
@@ -101,6 +101,7 @@ return [
     'created_at' => 'Created At',
     'updated_at' => 'Updated At',
     'schedule_at' => 'Schedule At',
+    'event' => 'Event',
     'action' => 'Action',
 
     'unauthorized_admin' => 'You do not have permission to access the admin area.',
