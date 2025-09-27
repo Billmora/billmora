ca08acd67729fba559b29ce2d424d65fe897b0bf
Mafly <maflycehaa@gmail.com>
2025-09-27 12:58:17 +0700

    feat(lang): add audit system logs translation

---
+++ lang/en_US/admin/audits/system.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'System Logs',
+    'description' => 'Review detailed records of system logs.',
+
+    'event_label' => 'Event',
+    'user_label' => 'User',
+    'properties_label' => 'Properties',
+];
