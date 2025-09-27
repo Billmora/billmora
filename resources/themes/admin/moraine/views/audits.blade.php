81ecc4d1bfa463163a85fb10cc088f9b07e53490
Mafly <maflycehaa@gmail.com>
2025-09-27 13:21:56 +0700

    feat: add shortcut to system log audit

--- resources/themes/admin/moraine/views/audits.blade.php
+++ resources/themes/admin/moraine/views/audits.blade.php
@@ -26,5 +26,14 @@
             </div>
         </a>
     @endcan
+    <a href="{{ route('admin.audits.system') }}" class="flex gap-4 items-center bg-white p-4 border-2 border-billmora-2 hover:border-billmora-primary rounded-2xl transition ease-in-out duration-150">
+        <div class="bg-billmora-primary p-2 rounded-full">
+            <x-lucide-history class="w-auto h-10 text-white" />
+        </div>
+        <div>
+        <h4 class="text-lg text-slate-700 font-semibold">{{ __('admin/audits/system.title') }}</h4> 
+        <p class="text-slate-500">{{ __('admin/audits/system.description') }}</p>
+        </div>
+    </a>
 </div>
 @endsection
