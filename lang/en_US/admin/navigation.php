77d16a30741685a1a72a63e9ff2c63fce6e87b0f
Nekoplii <maflycehaa@gmail.com>
2025-10-30 00:10:28 +0700

    feat(sidebar): add catalogs and product group to navigation

--- lang/en_US/admin/navigation.php
+++ lang/en_US/admin/navigation.php
@@ -3,10 +3,12 @@
 return [
     'dashboard' => 'Dashboard',
     'users' => 'Users',
+    'catalogs' => 'Catalogs',
     'settings' => 'Settings',
     'audits' => 'Audits',
     'group' => [
         'management' => 'Management',
+        'product' => 'Product',
         'system' => 'System',
     ],
 ];
--- resources/themes/admin/moraine/views/layouts/partials/sidebar.blade.php
+++ resources/themes/admin/moraine/views/layouts/partials/sidebar.blade.php
@@ -20,6 +20,11 @@
         <x-lucide-users class="w-5 h-auto" />
         <span class="font-semibold">{{ __('admin/navigation.users') }}</span>
       </a>
+      <span class="mt-4 block text-slate-600 font-semibold text-md">{{ __('admin/navigation.group.product') }}</span>
+      <a href="{{ route('admin.catalogs') }}" class="flex gap-2 items-center {{ request()->routeIs('admin.catalogs*') ? 'bg-billmora-primary text-white' : 'hover:bg-billmora-primary' }} px-3 py-3 rounded-lg text-slate-600 hover:text-white transition-colors duration-300">
+        <x-lucide-box class="w-5 h-auto" />
+        <span class="font-semibold">{{ __('admin/navigation.catalogs') }}</span>
+      </a>
       <span class="mt-4 block text-slate-600 font-semibold text-md">{{ __('admin/navigation.group.system') }}</span>
       <a href="{{ route('admin.settings') }}" class="flex gap-2 items-center {{ request()->routeIs('admin.settings*') ? 'bg-billmora-primary text-white' : 'hover:bg-billmora-primary' }} px-3 py-3 rounded-lg text-slate-600 hover:text-white transition-colors duration-300">
         <x-lucide-settings class="w-5 h-auto" />
