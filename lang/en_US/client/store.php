311875fbc9b60f9e68d377f52c11da989f714c09
Nekoplii <maflycehaa@gmail.com>
2025-12-14 18:02:08 +0700

    feat(store): add some package label translation

--- lang/en_US/client/store.php
+++ lang/en_US/client/store.php
@@ -4,4 +4,12 @@ return [
     'view_package' => 'View Package',
     
     'order_now' => 'Order Now',
+
+    'package' => [
+        'billing_cycle' => 'Choose billing cycle',
+        'order_summary' => 'Order Summary',
+        'setup_fee' => 'Setup Fee',
+        'due_today' => 'Total Due Today',
+        'checkout' => 'Checkout',
+    ]
 ];
