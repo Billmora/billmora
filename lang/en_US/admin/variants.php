9691f306e1c50b1ef2cb8572a8a99b596a4eb12f
Nekoplii <maflycehaa@gmail.com>
2026-01-10 15:50:57 +0700

    feat(lang): add translation of pricing variant option

--- lang/en_US/admin/variants.php
+++ lang/en_US/admin/variants.php
@@ -24,4 +24,32 @@ return [
     'is_upgradable_helper' => 'Enable to allow this variant to be upgraded or changed after purchase.',
     'package_label' => 'Package',
     'package_helper' => 'Select the package this variant belongs to.',
+
+    'options' => [
+        'pricing_label' => 'Pricing',
+        'pricing_helper' => 'Manage the pricing options for this variant.',
+        'name_label' => 'Name',
+        'name_helper' => 'Enter the name of the variant option.',
+        'value_label' => 'Value',
+        'value_helper' => 'Enter the value of the variant option.',
+
+        'pricing' => [
+            'name_label' => 'Name',
+            'name_helper' => 'Enter the name for this pricing option.',
+            'type_label' => 'Type',
+            'type_helper' => 'Select the type of pricing for this variant option.',
+            'time_interval_label' => 'Time Interval',
+            'time_interval_helper' => 'Choose the billing interval for this pricing option.',
+            'billing_period_label' => 'Billing Period',
+            'billing_period_helper' => 'Select how often the customer will be billed for this variant option.',
+            'currency_code_label' => 'Currency Code',
+            'currency_code_helper' => 'Displays the currency used for this price. This value cannot be changed here.',
+            'price_label' => 'Price',
+            'price_helper' => 'Set the price for this variant option in the selected currency.',
+            'setup_fee_label' => 'Setup Fee',
+            'setup_fee_helper' => 'Enter any one-time setup fee for this pricing option.',
+            'enabled_label' => 'Enabled?',
+            'enabled_helper' => 'Enable or disable this pricing option without deleting it.',
+        ]
+    ]
 ];
