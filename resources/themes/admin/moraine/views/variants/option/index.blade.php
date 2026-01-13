724845956022a34fe311d6603820df7587119681
Nekoplii <maflycehaa@gmail.com>
2026-01-13 17:54:58 +0700

    feat(variants): insert edit route in action of table list

--- resources/themes/admin/moraine/views/variants/option/index.blade.php
+++ resources/themes/admin/moraine/views/variants/option/index.blade.php
@@ -45,10 +45,10 @@
                             <td class="px-6 py-4 whitespace-nowrap text-sm text-slate-800">{{ $option->value }}</td>
                             <td class="px-6 py-4 whitespace-nowrap text-sm text-slate-800">{{ $option->created_at }}</td>
                             <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium space-x-2">
-                                    <a href="#" class="inline-flex items-center text-sm font-semibold text-billmora-primary hover:text-billmora-primary-hover">
-                                        {{ __('common.edit') }}
-                                    </a>
-                                    <x-admin::modal.trigger modal="deleteModal-{{ $option->id }}" variant="open" class="inline-flex items-center text-sm font-semibold text-red-400 hover:text-red-500 cursor-pointer">{{ __('common.delete') }}</x-admin::modal.trigger>
+                                <a href="{{ route('admin.variants.options.edit', ['id' => $variant->id, 'option' => $option->id]) }}" class="inline-flex items-center text-sm font-semibold text-billmora-primary hover:text-billmora-primary-hover">
+                                    {{ __('common.edit') }}
+                                </a>
+                                <x-admin::modal.trigger modal="deleteModal-{{ $option->id }}" variant="open" class="inline-flex items-center text-sm font-semibold text-red-400 hover:text-red-500 cursor-pointer">{{ __('common.delete') }}</x-admin::modal.trigger>
                             </td>
                         </tr>
                         @endforeach
