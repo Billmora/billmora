c4bd64614954c552d5c572968310fae266d3b2a2
Nekoplii <maflycehaa@gmail.com>
2026-01-17 10:15:19 +0700

    feat(coupons): insert coupons navigation into sidebar

--- resources/themes/admin/moraine/views/layouts/partials/sidebar.blade.php
+++ resources/themes/admin/moraine/views/layouts/partials/sidebar.blade.php
@@ -33,6 +33,10 @@
         <x-lucide-boxes class="w-5 h-auto" />
         <span class="font-semibold">{{ __('admin/navigation.variants') }}</span>
       </a>
+      <a href="{{ route('admin.coupons') }}" class="flex gap-2 items-center {{ request()->routeIs('admin.coupons*') ? 'bg-billmora-primary text-white' : 'hover:bg-billmora-primary' }} px-3 py-3 rounded-lg text-slate-600 hover:text-white transition-colors duration-300">
+        <x-lucide-tags class="w-5 h-auto" />
+        <span class="font-semibold">{{ __('admin/navigation.coupons') }}</span>
+      </a>
       <span class="mt-4 block text-slate-600 font-semibold text-md">{{ __('admin/navigation.group.system') }}</span>
       <a href="{{ route('admin.settings') }}" class="flex gap-2 items-center {{ request()->routeIs('admin.settings*') ? 'bg-billmora-primary text-white' : 'hover:bg-billmora-primary' }} px-3 py-3 rounded-lg text-slate-600 hover:text-white transition-colors duration-300">
         <x-lucide-settings class="w-5 h-auto" />
