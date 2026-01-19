7e20df9e4a8af15fa8961796c4340459fc22dcfd
Nekoplii <maflycehaa@gmail.com>
2026-01-19 12:04:09 +0700

    feat(coupon): insert coupon table into coupon_usages

--- database/migrations/2026_01_17_160155_create_coupon_usages_table.php
+++ database/migrations/2026_01_17_160155_create_coupon_usages_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('coupon_usages', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('coupon_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamp('used_at');
             $table->timestamps();
         });
     }
