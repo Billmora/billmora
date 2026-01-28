abad69ab7028c30ec14e3f17b3f12098b6acf1d3
Nekoplii <maflycehaa@gmail.com>
2026-01-29 00:00:32 +0700

    fix(client): overlapping component on sidebar

--- resources/themes/client/moraine/views/layouts/partials/sidebar.blade.php
+++ resources/themes/client/moraine/views/layouts/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-<nav id="sidebar" class="fixed z-10 xl:sticky top-0 left-0 xl:block shrink-0 p-5 xl:pr-0 w-[350px] sm:w-[400px] h-dvh -translate-x-full xl:translate-x-0 transition-transform duration-300 ease-in-out">
+<nav id="sidebar" class="fixed z-100 xl:sticky top-0 left-0 xl:block shrink-0 p-5 xl:pr-0 w-[350px] sm:w-[400px] h-dvh -translate-x-full xl:translate-x-0 transition-transform duration-300 ease-in-out">
   <div class="bg-white flex flex-col w-full h-full border-2 border-billmora-2 rounded-2xl p-8">
     <a href="{{ route('client.dashboard') }}" class="relative flex gap-3 items-center">
       <img src="{{ Billmora::getGeneral('company_logo') }}" alt="billmora logo" class="w-auto h-11 rounded-lg">
@@ -21,7 +21,6 @@
         <x-lucide-store class="w-5 h-auto" />
         <span class="font-semibold">{{ __('client/navigation.store') }}</span>
       </a>
-      {{-- <span class="mt-4 block text-slate-600 font-semibold text-md">{{ __('client/navigation.group.system') }}</span> --}}
     </div>
     @guest
       <div class="grid sm:hidden grid-cols-2 gap-4 mt-auto pt-4">
@@ -36,4 +35,4 @@
   </div>
 </nav>
 <!-- Backdrop -->
-<div id="backdrop" class="fixed inset-0 bg-black/25 z-5 xl:hidden opacity-0 pointer-events-none transition-opacity duration-300"></div>
+<div id="backdrop" class="fixed inset-0 bg-black/25 z-99 xl:hidden opacity-0 pointer-events-none transition-opacity duration-300"></div>
