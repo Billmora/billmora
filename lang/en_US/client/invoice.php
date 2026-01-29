a7c3b0c574ef98cd3fdb04f27bacc65e8e805785
Nekoplii <maflycehaa@gmail.com>
2026-01-29 14:21:16 +0700

    feat(lang): add invoice detail translation

--- lang/en_US/client/invoice.php
+++ lang/en_US/client/invoice.php
@@ -6,4 +6,15 @@ return [
     'invoice_date' => 'Invoice Date',
     'due_date' => 'Due Date',
     'total' => 'Total',
+
+    'invoice_label' => 'Invoice #:number',
+    'bill_to' => 'Bill To',
+    'issued_to' => 'Issued To',
+    'invoice_items' => 'Invoice Items',
+    'description' => 'Description',
+    'quantity' => 'Quantity',
+    'price' => 'Price',
+    'price' => 'Price',
+    'amount' => 'Amount',
+    'total_due' => 'Total Due',
 ];
