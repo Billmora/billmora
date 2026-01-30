d364815171da3c52b9b3797f80ad515c49d7553f
Nekoplii <maflycehaa@gmail.com>
2026-01-31 02:07:08 +0700

    feat(lang): add invoices translation for admin

---
+++ lang/en_US/admin/invoices.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'invoice_number_label' => 'Invoice Number',
+    'invoice_date_label' => 'Invoice Date',
+    'due_date_label' => 'Due Date',
+    'total_label' => 'Total',
+    'manage_label' => 'Manage',
+];
