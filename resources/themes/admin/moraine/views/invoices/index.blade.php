2e20592bfc1acfa5856579fef19d31822af02ac5
Nekoplii <maflycehaa@gmail.com>
2026-01-31 19:28:55 +0700

    feat(invoices): insert route invoice creation in admin

--- resources/themes/admin/moraine/views/invoices/index.blade.php
+++ resources/themes/admin/moraine/views/invoices/index.blade.php
@@ -5,7 +5,7 @@
 @section('body')
 <div class="flex flex-col gap-4">
     <div class="flex flex-col md:flex-row gap-4 justify-between items-center">
-        <a href="#" class="flex gap-1 items-center bg-billmora-primary hover:bg-billmora-primary-hover px-3 py-2 ml-auto text-white rounded-lg transition-colors ease-in-out duration-150 cursor-pointer">
+        <a href="{{ route('admin.invoices.create') }}" class="flex gap-1 items-center bg-billmora-primary hover:bg-billmora-primary-hover px-3 py-2 ml-auto text-white rounded-lg transition-colors ease-in-out duration-150 cursor-pointer">
             <x-lucide-plus class="w-auto h-5" />
             {{ __('common.create') }}
         </a>
