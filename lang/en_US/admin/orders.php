69f7e6a01192f0e0b5b0d9ac446f861dfb4363c1
Nekoplii <maflycehaa@gmail.com>
2026-02-02 12:09:06 +0700

    feat(lang): add orders translation for admin

---
+++ lang/en_US/admin/orders.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'number_label' => 'Order Number',
+    'user_label' => 'User',
+    'status_label' => 'Status',
+    'date_label' => 'Order Date',
+    'total_label' => 'Total',
+];
