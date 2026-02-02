707b153eca6b9ab7e64654708e9a1522c18d62c9
Nekoplii <maflycehaa@gmail.com>
2026-02-02 12:11:18 +0700

    feat(admin): add route manage orders to navigation sidebar

--- resources/themes/admin/moraine/views/layouts/partials/sidebar.blade.php
+++ resources/themes/admin/moraine/views/layouts/partials/sidebar.blade.php
@@ -24,6 +24,10 @@
         <x-lucide-receipt-text class="w-5 h-auto" />
         <span class="font-semibold">{{ __('admin/navigation.invoices') }}</span>
       </a>
+      <a href="{{ route('admin.orders') }}" class="flex gap-2 items-center {{ request()->routeIs('admin.orders*') ? 'bg-billmora-primary text-white' : 'hover:bg-billmora-primary' }} px-3 py-3 rounded-lg text-slate-600 hover:text-white transition-colors duration-300">
+        <x-lucide-shopping-bag class="w-5 h-auto" />
+        <span class="font-semibold">{{ __('admin/navigation.orders') }}</span>
+      </a>
       <span class="mt-4 block text-slate-600 font-semibold text-md">{{ __('admin/navigation.group.product') }}</span>
       <a href="{{ route('admin.catalogs') }}" class="flex gap-2 items-center {{ request()->routeIs('admin.catalogs*') ? 'bg-billmora-primary text-white' : 'hover:bg-billmora-primary' }} px-3 py-3 rounded-lg text-slate-600 hover:text-white transition-colors duration-300">
         <x-lucide-box class="w-5 h-auto" />
