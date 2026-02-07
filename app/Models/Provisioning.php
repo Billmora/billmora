9ab048c21f9a7dc39ce51f13bd422be841cc190d
Nekoplii <maflycehaa@gmail.com>
2026-02-07 19:47:58 +0700

    feat: add model Provisioning with migration file

---
+++ app/Models/Provisioning.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Provisioning extends Model
+{
+    protected $table = 'provisionings';
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @var list<string>
+     */
+    protected $casts = [
+        'is_active' => 'boolean',
+        'meta_data' => 'array', 
+    ];
+}
