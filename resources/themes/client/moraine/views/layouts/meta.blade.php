1c9cf887fc1a5c7e33d759a9c36a25e50d68b80f
Nekoplii <maflycehaa@gmail.com>
2026-02-09 20:07:59 +0700

    feat(client): insert fontawesome stylesheet icon to meta

--- resources/themes/client/moraine/views/layouts/meta.blade.php
+++ resources/themes/client/moraine/views/layouts/meta.blade.php
@@ -5,5 +5,6 @@
 <title>@yield('title', 'Client Area') | {{ Billmora::getGeneral('company_name') }}</title>
 <!-- Styles -->
 <link rel="stylesheet" href="{{ $clientTheme['assets'] }}/css/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script src="{{ $clientTheme['assets'] }}/js/app.js" type="module"></script>
 @livewireStyles
