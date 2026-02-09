56a641fb29669be81db4257e2bb36178b9007451
Nekoplii <maflycehaa@gmail.com>
2026-02-09 12:04:21 +0700

    feat(client): add translation for service fields

---
+++ lang/en_US/client/service.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'package_label' => 'Package',
+    'category_label' => 'Category',
+    'billing_cycle_label' => 'Billing cycle',
+    'price_label' => 'Price',
+    'expires_label' => 'Expires At',
+];
