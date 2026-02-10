7c6aeadfef2b713016aabb38b62d5945095a7064
Nekoplii <maflycehaa@gmail.com>
2026-02-10 19:35:24 +0700

    refactor(lang): rename label of category to catalog

--- lang/en_US/client/services.php
+++ lang/en_US/client/services.php
@@ -2,7 +2,7 @@
 
 return [
     'package_label' => 'Package',
-    'category_label' => 'Category',
+    'catalog_label' => 'Catalog',
     'registration_label' => 'Registration Date',
     'billing_cycle_label' => 'Billing Cycle',
     'price_label' => 'Price',
--- resources/themes/client/moraine/views/services/index.blade.php
+++ resources/themes/client/moraine/views/services/index.blade.php
@@ -10,7 +10,7 @@
                 <thead class="bg-billmora-2">
                     <tr>
                         <th scope="col" class="px-6 py-4 text-start text-xs font-semibold text-slate-500 uppercase">{{ __('client/services.package_label') }}</th>
-                        <th scope="col" class="px-6 py-4 text-start text-xs font-semibold text-slate-500 uppercase">{{ __('client/services.category_label') }}</th>
+                        <th scope="col" class="px-6 py-4 text-start text-xs font-semibold text-slate-500 uppercase">{{ __('client/services.catalog_label') }}</th>
                         <th scope="col" class="px-6 py-4 text-start text-xs font-semibold text-slate-500 uppercase">{{ __('client/services.billing_cycle_label') }}</th>
                         <th scope="col" class="px-6 py-4 text-start text-xs font-semibold text-slate-500 uppercase">{{ __('client/services.price_label') }}</th>
                         <th scope="col" class="px-6 py-4 text-start text-xs font-semibold text-slate-500 uppercase">{{ __('client/services.expires_label') }}</th>
