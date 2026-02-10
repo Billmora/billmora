f5902d8688de5989a5a40e8fe6cb1cbb82441416
Nekoplii <maflycehaa@gmail.com>
2026-02-10 19:43:39 +0700

    feat(lang): add services translation for admin

---
+++ lang/en_US/admin/services.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'package_label' => 'Package',
+    'catalog_label' => 'Catalog',
+    'user_label' => 'User',
+    'price_label' => 'Price',
+    'expires_label' => 'Expires At',
+];
