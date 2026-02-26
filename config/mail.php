4df6d27499db08eb21d42b17e09593d80602a421
Nekoplii <maflycehaa@gmail.com>
2026-02-26 16:29:16 +0700

    feat(config): add mailgun mailer configuration and register env variables in env.example

--- config/mail.php
+++ config/mail.php
@@ -49,6 +49,12 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
+        'mailgun' => [
+            'domain' => env('MAILGUN_DOMAIN'),
+            'secret' => env('MAILGUN_SECRET'),
+            'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        ],
+
         'ses' => [
             'transport' => 'ses',
         ],
