e2faa354291c3ada9dc6eb886377e20f897314dc
Nekoplii <maflycehaa@gmail.com>
2026-02-27 14:12:06 +0700

    feat(lang): add translation for ticket fields

---
+++ lang/en_US/admin/tickets.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'ticket_number_label' => 'Ticket Number',
+    'ticket_subject_label' => 'Subject',
+    'ticket_subject_helper' => 'Enter a brief subject that describes the ticket topic.',
+    'ticket_status_label' => 'Status',
+    'ticket_status_helper' => 'Set the current status of the ticket.',
+    'ticket_priority_label' => 'Priority',
+    'ticket_priority_helper' => 'Set the priority level to indicate the urgency of this ticket.',
+    'ticket_department_label' => 'Department',
+    'ticket_department_helper' => 'Select the department responsible for handling this ticket.',
+    'ticket_user_label' => 'Client',
+    'ticket_user_helper' => 'Select the client this ticket is being created for.',
+    'ticket_assigned_label' => 'Assigned To',
+    'ticket_assigned_helper' => 'Select the staff member assigned to handle this ticket.',
+    'ticket_service_label' => 'Related Service',
+    'ticket_service_helper' => 'Select the service related to this ticket, if applicable.',
+];
