203b091721185bfd997ac860c5a801929a32087c
Nekoplii <maflycehaa@gmail.com>
2026-03-05 20:22:50 +0700

    feat(lang): add service scaling message and label

--- lang/en_US/client/services.php
+++ lang/en_US/client/services.php
@@ -11,6 +11,9 @@ return [
     'variant_label' => 'Variant Options',
     'configuration_label' => 'Additional Configuration',
     'cancel_label' => 'Cancellation Request',
+    'scale_label' => 'Scaling Service',
+    'scale_package_helper' => 'Choose a package want to scale to',
+    'scale_variant_helper' => 'Choose a variant want to scale to',
 
     'action' => [
         'overview' => 'Overview',
@@ -29,6 +32,18 @@ return [
 
         'requested' => 'Cancellation request has been submitted.',
         'pending' => 'This service has a pending cancellation request. Please contact support if you wish to withdraw it.',
+    ],
+
+    'scaling' => [
+        'must_be_active' => 'Only active services can be scaled.',
+        'unpaid_invoice_exists' => 'Please pay all outstanding invoices before making changes to this service.',
+        'service_overdue' => 'Your service has expired. Please renew it before requesting a scale.',
+        'invalid_package' => 'The selected package is not valid for this service.',
+        'no_variants' => 'This package has no variant options.',
+        'no_variant_changes' => 'Please change at least one option to proceed.',
+        'downgrade_success' => 'Your service has been downgraded and will take effect on the next billing cycle.',
+        'upgrade_success' => 'Upgrade invoice has been created. Complete your payment to apply the changes.',
+        'invoice_item_description' => ':old_package scale to :new_package (Prorated for :days day(s))',
     ],
 
     'provisioning' => [
