767183b328b24a21cd947d3a640d3ddc9a35ce62
Nekoplii <maflycehaa@gmail.com>
2026-03-09 15:02:59 +0700

    refactor(lang): rename quick_search to browse

--- lang/en_US/admin/common.php
+++ lang/en_US/admin/common.php
@@ -2,11 +2,11 @@
 
 return [
     'search' => 'Search...',
-    'quick_search' => 'Quick Search...',
-    'quick_search_not_found' => 'No results found.',
-    'quick_search_navigate' => 'to navigate',
-    'quick_search_select' => 'to select',
-    'quick_search_close' => 'to close',
+    'browse' => 'Browse...',
+    'browse_not_found' => 'No results found.',
+    'browse_navigate' => 'to navigate',
+    'browse_select' => 'to select',
+    'browse_close' => 'to close',
 
     'none' => 'None',
     'key' => 'Key',
