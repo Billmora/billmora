22ffebe82b523a148b26419c363df8ac896e58c4
Nekoplii <maflycehaa@gmail.com>
2026-03-12 15:18:28 +0700

    feat(lang): add credit label and message translation

--- lang/en_US/client/account.php
+++ lang/en_US/client/account.php
@@ -8,4 +8,16 @@ return [
     'current_password' => 'Current Password',
     'new_password' => 'New Password',
     'confirm_new_password' => 'Confirm New Password',
+
+    'credits' => [
+        'currency_label' => 'Currency',
+        'amount_label' => 'Amount',
+        'payment_method_label' => 'Payment Method',
+
+        'no_credits' => 'You have no credits.',
+        'topup_submit' => 'Add Funds',
+        'topup_success' => 'Invoice for adding funds has been generated. Please complete your payment.',
+        'insufficient_balance' => 'Insufficient credit balance in :currency wallet.',
+        'topup_exceeds_max_balance' => 'This deposit exceeds your maximum allowed wallet balance of :max_balance.',
+    ],
 ];
