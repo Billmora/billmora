b4632c529d5bbb6ad33072a89755d378ef83af6d
Nekoplii <maflycehaa@gmail.com>
2026-03-14 20:02:58 +0700

    feat(theme): add JSON manifest accessor and static caching

--- app/Models/Theme.php
+++ app/Models/Theme.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
 
 class Theme extends Model
 {
+    protected static array $manifestCache = [];
+
     /**
      * The attributes that aren't mass assignable.
      *
@@ -27,4 +30,28 @@ class Theme extends Model
         'is_core' => 'boolean',
         'config' => 'array',
     ];
+
+    /**
+     * Get theme manifest data from theme.json file with caching.
+     *
+     * @return array<string, mixed>
+     */
+    public function getManifestAttribute(): array
+    {
+        $cacheKey = "{$this->type}-{$this->provider}";
+
+        if (isset(self::$manifestCache[$cacheKey])) {
+            return self::$manifestCache[$cacheKey];
+        }
+
+        $path = resource_path("themes/{$this->type}/{$this->provider}/theme.json");
+
+        if (File::exists($path)) {
+            $data = json_decode(File::get($path), true) ?? [];
+            self::$manifestCache[$cacheKey] = $data;
+            return $data;
+        }
+
+        return [];
+    }
 }
