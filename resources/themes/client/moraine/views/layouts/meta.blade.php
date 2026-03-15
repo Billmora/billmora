5afa5625716c342dbc2e0b118655303c526824c4
Nekoplii <maflycehaa@gmail.com>
2026-03-15 17:00:55 +0700

    refactor(client): remove prevent indexing in moraine theme

--- resources/themes/client/moraine/views/layouts/meta.blade.php
+++ resources/themes/client/moraine/views/layouts/meta.blade.php
@@ -1,5 +1,3 @@
-<!-- Prevent indexing -->
-<meta name="robots" content="noindex, nofollow">
 {{-- Meta --}}
 <link rel="icon" href="https://media.billmora.com/logo/main-bgnone.svg">
 <title>@yield('title', 'Client Area') | {{ Billmora::getGeneral('company_name') }}</title>
