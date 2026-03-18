9211961962d26a17abed3249a58c78648aaccf4f
Nekoplii <maflycehaa@gmail.com>
2026-03-19 00:13:23 +0700

    feat(settings): insert navigation of tax into settings list

--- resources/themes/admin/moraine/views/settings/index.blade.php
+++ resources/themes/admin/moraine/views/settings/index.blade.php
@@ -70,6 +70,17 @@
             </div>
         </a>
     @endcan
+    @can('settings.taxes.view')
+        <a href="{{ route('admin.settings.taxes') }}" class="flex gap-4 items-center bg-white p-4 border-2 border-billmora-2 hover:border-billmora-primary-500 rounded-2xl transition ease-in-out duration-150">
+            <div class="bg-billmora-primary-500 p-2 rounded-full">
+                <x-lucide-receipt class="w-auto h-10 text-white" />
+            </div>
+            <div>
+            <h4 class="text-lg text-slate-700 font-semibold">{{ __('admin/settings/tax.title') }}</h4> 
+            <p class="text-slate-500">{{ __('admin/settings/tax.description') }}</p>
+            </div>
+        </a>
+    @endcan
     @can('settings.ticket.view')
         <a href="{{ route('admin.settings.ticket.ticketing') }}" class="flex gap-4 items-center bg-white p-4 border-2 border-billmora-2 hover:border-billmora-primary-500 rounded-2xl transition ease-in-out duration-150">
             <div class="bg-billmora-primary-500 p-2 rounded-full">
