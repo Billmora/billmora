7e0f3a7dfd838030dfac469f0e5538a36318d384
Mafly <110502739+Nekoplii@users.noreply.github.com>
2026-03-29 00:28:58 +0700

    New translations navigation.php (Portuguese, Brazilian)

---
+++ lang/pt_BR/admin/navigation.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'dashboard' => 'Dashboard',
+    'users' => 'Users',
+    'orders' => 'Orders',
+    'services' => 'Services',
+    'invoices' => 'Invoices',
+    'transactions' => 'Transactions',
+    'broadcasts' => 'Broadcasts',
+    'tickets' => 'Tickets',
+    'catalogs' => 'Catalogs',
+    'packages' => 'Packages',
+    'variants' => 'Variants',
+    'coupons' => 'Coupons',
+    'provisionings' => 'Provisionings',
+    'gateways' => 'Gateways',
+    'modules' => 'Modules',
+    'settings' => 'Settings',
+    'plugins' => 'Plugins',
+    'themes' => 'Themes',
+    'automations' => 'Automations',
+    'tasks' => 'Tasks',
+    'audits' => 'Audits',
+    'health' => 'Health',
+    'group' => [
+        'management' => 'Management',
+        'product' => 'Product',
+        'plugin' => 'Plugin',
+        'system' => 'System',
+    ],
+];
