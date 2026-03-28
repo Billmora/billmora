f1ea6af40cc1c99487c4a9f169415394d5460acf
Mafly <110502739+Nekoplii@users.noreply.github.com>
2026-03-29 00:42:59 +0700

    New translations dashboard.php (Afrikaans)

---
+++ lang/af_ZA/client/dashboard.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'active_services' => 'Active Services',
+    'unpaid_invoices' => 'Unpaid Invoices',
+    'open_tickets' => 'Open Tickets',
+
+    'order_new_services' => 'Order New Services',
+    'view_all' => 'View All',
+
+    'service_number' => 'Number',
+    'service_catalog' => 'Catalog',
+    'service_package' => 'Package',
+    'service_next_due_date' => 'Next Due Date',
+    'ticket_number' => 'Number',
+    'ticket_subject' => 'Subject',
+    'ticket_status' => 'Status',
+    'invoice_number' => 'Number',
+    'invoice_total' => 'Total',
+    'invoice_status' => 'Status',
+];
