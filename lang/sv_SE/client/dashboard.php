85601cdaec94c1108983b11b46fc31e3eef89b8f
Mafly <110502739+Nekoplii@users.noreply.github.com>
2026-03-29 08:19:54 +0700

    New translations dashboard.php (Swedish)

--- lang/sv_SE/client/dashboard.php
+++ lang/sv_SE/client/dashboard.php
@@ -1,21 +1,21 @@
 <?php
 
 return [
-    'active_services' => 'Active Services',
-    'unpaid_invoices' => 'Unpaid Invoices',
-    'open_tickets' => 'Open Tickets',
+    'active_services_label' => 'Active Services',
+    'unpaid_invoices_label' => 'Unpaid Invoices',
+    'open_tickets_label' => 'Open Tickets',
 
-    'order_new_services' => 'Order New Services',
-    'view_all' => 'View All',
+    'order_new_services_label' => 'Order New Services',
+    'view_all_label' => 'View All',
 
-    'service_number' => 'Number',
-    'service_catalog' => 'Catalog',
-    'service_package' => 'Package',
-    'service_next_due_date' => 'Next Due Date',
-    'ticket_number' => 'Number',
-    'ticket_subject' => 'Subject',
-    'ticket_status' => 'Status',
-    'invoice_number' => 'Number',
-    'invoice_total' => 'Total',
-    'invoice_status' => 'Status',
+    'service_number_label' => 'Number',
+    'service_catalog_label' => 'Catalog',
+    'service_package_label' => 'Package',
+    'service_next_due_date_label' => 'Next Due Date',
+    'ticket_number_label' => 'Number',
+    'ticket_subject_label' => 'Subject',
+    'ticket_status_label' => 'Status',
+    'invoice_number_label' => 'Number',
+    'invoice_total_label' => 'Total',
+    'invoice_status_label' => 'Status',
 ];
